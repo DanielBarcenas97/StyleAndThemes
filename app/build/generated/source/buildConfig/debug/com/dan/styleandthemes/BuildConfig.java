3894aea07ac517f73a162ed378fdee35d7fb7fbf
barcenas.proteco@gmail.com <Edgar1515>
2021-07-27 20:43:16 -0500

    subiendo temas y estilos

---
+++ app/build/generated/source/buildConfig/debug/com/dan/styleandthemes/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dan.styleandthemes;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dan.styleandthemes";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
