bab15e1381f7cab34aca507230a7d8ba37f51597
barcenas.proteco@gmail.com <Edgar1515>
2021-08-03 21:07:04 -0500

    -Radio button

--- app/src/main/java/com/dan/styleandthemes/MainActivity.java
+++ app/src/main/java/com/dan/styleandthemes/MainActivity.java
@@ -3,12 +3,46 @@ package com.dan.styleandthemes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioButton radioButtonMen;
+    RadioButton radioButtonWoman;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.formulario);
+        setContentView(R.layout.filters);
+
+        radioButtonMen = (RadioButton) findViewById(R.id.rb_men);
+        radioButtonWoman = (RadioButton) findViewById(R.id.rb_women);
+        textView = (TextView) findViewById(R.id.tv_search);
+
+        textView.setText(R.string.rb_women);
+
+
+        radioButtonMen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                radioButtonMen.setChecked(true);
+                textView.setText(R.string.rb_men);
+            }
+        });
+
+
+        radioButtonWoman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                radioButtonWoman.setChecked(true);
+                textView.setText(R.string.rb_women);
+            }
+        });
+
+
     }
+
 }
